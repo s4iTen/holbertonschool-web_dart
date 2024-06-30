4053d3730d68d8ef7376361019cef5865b14b037
s4iTen <ychouk102@gmail.com>
2024-06-30 16:16:28 +0200

    this function check if the args is higher ot equal to 80

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,12 +1,5 @@
 void main(List<String> args) {
-  if (args.isEmpty || int.tryParse(args[0]) == null) {
-    print('Please provide a valid integer argument.');
-    return;
-  }
-
-  int value = int.parse(args[0]);
-
-  if (value >= 80) {
-    print('you passed');
-  }
+  int nb = int.parse(args[0]);
+  assert(nb >= 80, 'The score must be bigger or equal to 80');
+  print('You passed');
 }
