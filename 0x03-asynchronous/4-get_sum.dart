c1923566dd60c7a0362a6c2beef7bd08fb0f7186
s4iTen <ychouk102@gmail.com>
2024-07-05 01:57:17 +0200

    this function fetch the user to get its id and fetch the order of this user and fitch the prices and calculate the total order price for this user

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,20 @@
+import '4-util.dart';
+import 'dart:convert';
+
+Future<String> calculateTotal() async {
+    try {
+        String userData = await fetchUserData();
+        Map<String, dynamic> user = jsonDecode(userData);
+        String id = user['id'];
+        String orders = await fetchUserOrders(id);
+        List<dynamic> products = jsonDecode(orders);
+        double total = 0;
+        for (var product in products) {
+            String price = await fetchProductPrice(product);
+            total += double.parse(price);
+        }
+        return '$total';
+    } catch (e) {
+        return 'error caught: $e';
+    }
+}
